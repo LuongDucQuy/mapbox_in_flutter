0c7e862234439e16009a91f961af25d82e74113c
dinhnguyenminhhoang <dinhnguyenminhhoang28@gmail.com>
2025-02-19 22:26:34 +0700

    [feat]:test

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Mapbox Demo',
+      title: 'Mapbox Demo 2',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
